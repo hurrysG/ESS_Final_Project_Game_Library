17ae3bf69cc67c9eba1c6704bcb8e60ab8099f6f
Yuanfu Tian <xaviertian89@gmail.com>
2021-12-16 17:54:31 -0500

    Update DataContext

--- FINALPROJ/Models/Persistence/DataContext.cs
+++ FINALPROJ/Models/Persistence/DataContext.cs
@@ -9,11 +9,15 @@ namespace FINALPROJ.Models
 {
     public class DataContext : DbContext
     {
-        public DbSet<User> Users{get; set;}
-        public DbSet<Game> Games{get; set;}
-    
+        public DbSet<User> Users { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Developer> Developers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
             optionsBuilder.UseSqlite("FileName=MyDB.db");
         }
     }
